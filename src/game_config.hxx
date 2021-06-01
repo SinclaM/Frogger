c6ca88f5146c46e139a299335b8c991f6aa78168
SinclaM <matthewsinclair2024@u.northwestern.edu>
2021-06-01 16:08:32 -0500

    Rearranged config

--- src/game_config.hxx
+++ src/game_config.hxx
@@ -31,8 +31,26 @@ struct Game_config{
     /// Amount frog moves per step in the x and y directions
     Dimension hop_dist;
 
+    /// The dimensions of coasters
+    Dimension coaster_small;
+    Dimension coaster_medium;
+    Dimension coaster_long;
+    Dimension coaster_longest;
+
+    /// Dimensions of the homes for the frog
+    Dimension home_dims;
+
+
+
     /// Starting position for frog (top center)
     Position start;
+
+
+
+    /// The water kill_zone
+    Rectangle kill_zone;
+
+
 
     /// The time a frog has to reach a home
     double lifetime;
@@ -40,17 +58,8 @@ struct Game_config{
     /// The minimum time between frog hops
     double hop_time;
 
-    /// The water kill_zone
-    Rectangle kill_zone;
-
     /// Time the frog waits, dead, before resetting
     double reset_wait_time;
-
-    /// Velocities of objects for each given row
-    std::vector<int> row_velocities;
-
-    /// Number of coasters in each row
-    std::vector<int> coaster_rows;
 
     /// Time for turtle submersion
     double turtle_sumberged_time;
@@ -61,29 +70,23 @@ struct Game_config{
     /// Time turtles are submerged for
     double turtle_sumbersed_for;
 
+    /// Fraction of the frog which must be colliding with a platform or home
+    /// before the frog will fall into the water. The most lenient collision
+    /// detection would have a value of 0 (the edges can touch and the frog
+    /// will still be on it). The most strict collision detection would have
+    /// a value of 1 (the frog must be entirely on the platform or it will
+    ///  fall off)
+    double frog_collision_fraction;
+
+
+
     /// Top of the first coaster lane
     int bottom_lane_y;
-
-    /// Default spacing between coasters for every lane (before random
-    /// deviation)
-    std::vector<int> spacings;
 
     /// Random deviation added to initial x position of cars, turtles, logs,
     /// etc. The deviation is a random number between -random_deviation_range
     /// and random_deviation_range
     int random_deviation_range;
-
-    /// The dimensions of coasters
-    Dimension coaster_small;
-    Dimension coaster_medium;
-    Dimension coaster_long;
-    Dimension coaster_longest;
-
-    /// Dimensions of the homes for the frog
-    Dimension home_dims;
-
-    /// Locations of the homes
-    std::vector<Position> home_locations;
 
     /// Points awarded for forward steps
     int forward_step_points;
@@ -102,41 +105,46 @@ struct Game_config{
     /// shift applied to all coasters in a row, but different for each row
     int shift_max;
 
-    /// Fraction of the frog which must be colliding with a platform or home
-    /// before the frog will fall into the water. The most lenient collision
-    /// detection would have a value of 0 (the edges can touch and the frog
-    /// will still be on it). The most strict collision detection would have
-    /// a value of 1 (the frog must be entirely on the platform or it will
-    ///  fall off)
-    double frog_collision_fraction;
+    /// Increase to velocities of coaster after a frog reaches a home
+    int velocity_gain;
+
+
+
+
+    /// Velocities of objects for each given row
+    std::vector<int> row_velocities;
+
+    /// Number of coasters in each row
+    std::vector<int> coaster_rows;
+
+
+    /// Default spacing between coasters for every lane (before random
+    /// deviation)
+    std::vector<int> spacings;
+
+
+    /// Locations of the homes
+    std::vector<Position> home_locations;
 
     //
     // Config for the View
     //
 
-    /// Color for the game-over background on loss
-    ge211::Color end_game_background_fade_loss;
-
-    /// Color for the game-over background on victory
-    ge211::Color end_game_background_fade_win;
+    /// Position of score sprite
+    Position score_pos;
 
     /// Position (top left) of leftmost life sprite
     Position leftmost_life_pos;
 
-    /// Horizontal spacing between life sprites
-    int life_spacing;
 
-    /// Position of score sprite
-    Position score_pos;
-
-    /// Increase to velocities of coaster after a frog reaches a home
-    int velocity_gain;
 
     /// Rectangle that will be used for do draw the timer
     Rectangle timer_rec;
 
-    /// Color that the timer will be drawn with
-    ge211::Color timer_color;
+
+
+    /// Horizontal spacing between life sprites
+    int life_spacing;
 
     /// Small font size for the view
     int small_font_size;
@@ -145,9 +153,23 @@ struct Game_config{
     int large_font_size;
 
 
+
+    /// Color for the game-over background on loss
+    ge211::Color end_game_background_fade_loss;
+
+    /// Color for the game-over background on victory
+    ge211::Color end_game_background_fade_win;
+
+    /// Color that the timer will be drawn with
+    ge211::Color timer_color;
+
+
     //
     // PUBLIC HELPERS
     //
+
+    /// Returns the row velocity in pixels per 4 frames
+    int row_velocity(int const) const;
 
     /// Returns whether a position is a valid position on screen for the frog
     /// to be in
@@ -155,9 +177,6 @@ struct Game_config{
 
     /// Returns whether a rectangle fits on screen
     bool in_scene(Rectangle const) const;
-
-    /// Returns the row velocity in pixels per 4 frames
-    int row_velocity(int const) const;
 
     /// Returns whether or not a position is outside the object scene, which
     /// is 105 pixels off either the left or right of the actual scene dims
